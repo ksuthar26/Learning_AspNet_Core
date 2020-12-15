36db557259146688c18533b5cee037a71eb91ed6
Kirtesh Suthar <Kirtesh@yahoo.com>
2020-12-15 17:05:27 +0530

    Static file , default files and directory browsing...

--- Learning_AspNet_Core/Learning_AspNet_Core/Startup.cs
+++ Learning_AspNet_Core/Learning_AspNet_Core/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +18,7 @@ namespace Learning_AspNet_Core
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +28,54 @@ namespace Learning_AspNet_Core
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("Custom.html");
+
+
+            //app.UseDefaultFiles(defaultFilesOptions);
+
+            //app.UseStaticFiles();
+
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Css")),
+            //    RequestPath = new PathString("/MyCss")
+            //});
+
+            //app.UseDirectoryBrowser();
+
+            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //        Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Css")),
+            //    RequestPath = new PathString("/MyCss")
+            //});
+
+            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //        Path.Combine(Directory.GetCurrentDirectory(), @"MyDirectory")),
+            //    RequestPath = new PathString("/MyDirectory")
+            //});
+
+            app.UseFileServer(enableDirectoryBrowsing: true);
+
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("Custom.html");
+
+            app.UseFileServer(fileServerOptions);
+
+            app.UseFileServer(new FileServerOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+           Path.Combine(Directory.GetCurrentDirectory(), @"MyDirectory")),
+                RequestPath = new PathString("/MyDirectory"),
+                EnableDirectoryBrowsing = true
+            });
 
             app.UseRouting();
 
