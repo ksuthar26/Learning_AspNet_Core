39aed41a0d502ed79f3605843d40659134d82ea9
Kirtesh Suthar <Kirtesh@yahoo.com>
2020-12-16 22:30:39 +0530

    Understood Program.cs and Startup.cs

--- Learning_AspNet_Core/Learning_AspNet_Core/Program.cs
+++ Learning_AspNet_Core/Learning_AspNet_Core/Program.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,11 +19,44 @@ namespace Learning_AspNet_Core
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                // .UseContentRoot(Path.Combine(Directory.GetCurrentDirectory(), "MyDirectory"))
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    //configHost.Sources.Clear();
+
+                    configHost.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "MyHost"));
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    //configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    //configHost.AddCommandLine(args);
+                })
+                .UseEnvironment("Development 123")
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "MyHost"));
+                    config.AddJsonFile("MySettings.json", optional: true, reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+
+                    var config = new ConfigurationBuilder()
+                                     .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "MyHost"))
+                                     .AddJsonFile("hostsettings.json", optional: true)
+                                     .AddCommandLine(args)
+                                     .Build();
+
+                    //webBuilder.CaptureStartupErrors(true);                    
+                    webBuilder.UseSetting("APPLICATIONNAME", "CustomApplicationName");
+                    // webBuilder.UseContentRoot(Path.Combine(Directory.GetCurrentDirectory(), "MyContent"));
+                    // webBuilder.UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "MyWeb"));
+                    webBuilder.UseEnvironment("gfdfgf");   
+                    //webBuilder.UseConfiguration(config);
+
                     webBuilder.UseStartup<Startup>();
                 });
+        }
+
     }
 }
--- Learning_AspNet_Core/Learning_AspNet_Core/Startup.cs
+++ Learning_AspNet_Core/Learning_AspNet_Core/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
@@ -22,60 +23,28 @@ namespace Learning_AspNet_Core
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration configration)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
-            //defaultFilesOptions.DefaultFileNames.Clear();
-            //defaultFilesOptions.DefaultFileNames.Add("Custom.html");
+            // app.UseFileServer(enableDirectoryBrowsing: true);
 
+            // FileServerOptions fileServerOptions = new FileServerOptions();
+            // fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            // fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("Custom.html");
 
-            //app.UseDefaultFiles(defaultFilesOptions);
+            // app.UseFileServer(fileServerOptions);
 
-            //app.UseStaticFiles();
-
-            //app.UseStaticFiles(new StaticFileOptions()
-            //{
-            //    FileProvider = new PhysicalFileProvider(
-            //Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Css")),
-            //    RequestPath = new PathString("/MyCss")
-            //});
-
-            //app.UseDirectoryBrowser();
-
-            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
-            //{
-            //    FileProvider = new PhysicalFileProvider(
-            //        Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\Css")),
-            //    RequestPath = new PathString("/MyCss")
-            //});
-
-            //app.UseDirectoryBrowser(new DirectoryBrowserOptions()
-            //{
-            //    FileProvider = new PhysicalFileProvider(
-            //        Path.Combine(Directory.GetCurrentDirectory(), @"MyDirectory")),
-            //    RequestPath = new PathString("/MyDirectory")
-            //});
-
-            app.UseFileServer(enableDirectoryBrowsing: true);
-
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("Custom.html");
-
-            app.UseFileServer(fileServerOptions);
-
-            app.UseFileServer(new FileServerOptions()
-            {
-                FileProvider = new PhysicalFileProvider(
-           Path.Combine(Directory.GetCurrentDirectory(), @"MyDirectory")),
-                RequestPath = new PathString("/MyDirectory"),
-                EnableDirectoryBrowsing = true
-            });
+            // app.UseFileServer(new FileServerOptions()
+            // {
+            //     FileProvider = new PhysicalFileProvider(
+            //Path.Combine(Directory.GetCurrentDirectory(), @"MyDirectory")),
+            //     RequestPath = new PathString("/MyDirectory"),
+            //     EnableDirectoryBrowsing = true
+            // });
 
             app.UseRouting();
 
@@ -83,7 +52,7 @@ namespace Learning_AspNet_Core
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync($" My Key : {configration.GetValue<string>("MyKey")} MMM : {configration.GetValue<string>("MMM")}  App Name : {env.ApplicationName} Content Root: { env.ContentRootPath} Env Name : {env.EnvironmentName}  Web Root: { env.WebRootPath}   Hello World!");
                 });
             });
         }
